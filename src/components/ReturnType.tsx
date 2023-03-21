c630b116d8ade7a067e03956c14f772365b1fa05
rsourdis <rsourdis@gmail.com>
2023-03-21 15:12:03 +0100

    Adding Return Label Printing EU 1.0

--- src/components/ReturnType.tsx
+++ src/components/ReturnType.tsx
@@ -1,13 +1,5 @@
 import React, { useContext, useState } from 'react'
-import {
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Row,
-  Spinner,
-} from 'reactstrap'
+import { Button, Card, CardBody, CardHeader, Col, Row, Spinner } from 'reactstrap'
 // import Animation from '@components/Common/Animation'
 import { OrderRowType, ShipmentOrderItem } from '@typings'
 import axios from 'axios'
@@ -20,7 +12,7 @@ import { useSWRConfig } from 'swr'
 // });
 
 type Props = {
-  data: OrderRowType,
+  data: OrderRowType
   apiMutateLink?: string
 }
 
@@ -51,33 +43,28 @@ const ReturnType = ({ data, apiMutateLink }: Props) => {
         <Col xl={4}>
           <Col xl={12}>
             <Card>
-              <CardHeader className="py-3">
-                <h5 className="fw-semibold m-0">Shipping</h5>
+              <CardHeader className='py-3'>
+                <h5 className='fw-semibold m-0'>Shipping</h5>
               </CardHeader>
               <CardBody>
-                <table className="table table-sm table-borderless">
+                <table className='table table-sm table-borderless'>
                   <tbody>
                     <tr>
-                      <td className="text-muted text-nowrap">
-                        Service Requested:
-                      </td>
-                      <td className="fw-semibold w-100">
-                        {data.carrierService}
-                      </td>
+                      <td className='text-muted text-nowrap'>Service Requested:</td>
+                      <td className='fw-semibold w-100'>{data.carrierService}</td>
                     </tr>
                     <tr>
-                      <td className="text-muted text-nowrap">Service Used:</td>
-                      <td className="fw-semibold w-100">{data.carrierType}</td>
+                      <td className='text-muted text-nowrap'>Service Used:</td>
+                      <td className='fw-semibold w-100'>{data.carrierType}</td>
                     </tr>
                     <tr>
-                      <td className="text-muted text-nowrap">Customer Name:</td>
-                      <td className="fw-semibold w-100">{data.shipName}</td>
+                      <td className='text-muted text-nowrap'>Customer Name:</td>
+                      <td className='fw-semibold w-100'>{data.shipName}</td>
                     </tr>
                     <tr>
-                      <td className="text-muted text-nowrap">Address:</td>
-                      <td className="fw-semibold w-100">
-                        {data.shipStreet}, {data.shipCity}, {data.shipState},{' '}
-                        {data.shipZipcode}, {data.shipCountry}
+                      <td className='text-muted text-nowrap'>Address:</td>
+                      <td className='fw-semibold w-100'>
+                        {data.shipStreet}, {data.shipCity}, {data.shipState}, {data.shipZipcode}, {data.shipCountry}
                       </td>
                     </tr>
                   </tbody>
@@ -87,35 +74,27 @@ const ReturnType = ({ data, apiMutateLink }: Props) => {
           </Col>
           <Col xl={12}>
             <Card>
-              <CardHeader className="py-3">
-                <h5 className="fw-semibold m-0">Charge Details</h5>
+              <CardHeader className='py-3'>
+                <h5 className='fw-semibold m-0'>Charge Details</h5>
               </CardHeader>
               <CardBody>
-                <table className="table table-sm table-borderless table-nowrap mb-0">
+                <table className='table table-sm table-borderless table-nowrap mb-0'>
                   <tbody>
-                    <tr className="border-bottom pb-2">
-                      <td className="text-muted">Pick Pack Charge</td>
-                      <td className="fw-semibold text-end">
-                        $ {data.pickpackCharge.toFixed(2)}
-                      </td>
+                    <tr className='border-bottom pb-2'>
+                      <td className='text-muted'>Pick Pack Charge</td>
+                      <td className='fw-semibold text-end'>$ {data.pickpackCharge.toFixed(2)}</td>
                     </tr>
-                    <tr className="border-bottom pb-2">
-                      <td className="text-muted">Shipping Charge</td>
-                      <td className="fw-semibold text-end">
-                        $ {data.onixShipping.toFixed(2)}
-                      </td>
+                    <tr className='border-bottom pb-2'>
+                      <td className='text-muted'>Shipping Charge</td>
+                      <td className='fw-semibold text-end'>$ {data.onixShipping.toFixed(2)}</td>
                     </tr>
-                    <tr className="border-bottom pb-2">
-                      <td className="text-muted">Extra Charge</td>
-                      <td className="fw-semibold text-end">
-                        $ {data.extraCharge.toFixed(2)}
-                      </td>
+                    <tr className='border-bottom pb-2'>
+                      <td className='text-muted'>Extra Charge</td>
+                      <td className='fw-semibold text-end'>$ {data.extraCharge.toFixed(2)}</td>
                     </tr>
                     <tr>
-                      <td className="fw-bold">TOTAL</td>
-                      <td className="text-primary fw-semibold text-end">
-                        $ {data.totalCharge.toFixed(2)}
-                      </td>
+                      <td className='fw-bold'>TOTAL</td>
+                      <td className='text-primary fw-semibold text-end'>$ {data.totalCharge.toFixed(2)}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -125,8 +104,8 @@ const ReturnType = ({ data, apiMutateLink }: Props) => {
           {data.extraComment != '' && (
             <Col xl={12}>
               <Card>
-                <CardHeader className="py-3">
-                  <h5 className="fw-semibold m-0">Order Comment</h5>
+                <CardHeader className='py-3'>
+                  <h5 className='fw-semibold m-0'>Order Comment</h5>
                 </CardHeader>
                 <CardBody>
                   <p>{data.extraComment}</p>
@@ -137,46 +116,38 @@ const ReturnType = ({ data, apiMutateLink }: Props) => {
         </Col>
         <Col xl={8}>
           <Card>
-            <CardHeader className="py-3">
-              <h5 className="fw-semibold m-0">Products</h5>
+            <CardHeader className='py-3'>
+              <h5 className='fw-semibold m-0'>Products</h5>
             </CardHeader>
             <CardBody>
-              <div className="table-responsive">
-                <table className="table table-sm align-middle table-borderless mb-0">
-                  <thead className="table-light">
+              <div className='table-responsive'>
+                <table className='table table-sm align-middle table-borderless mb-0'>
+                  <thead className='table-light'>
                     <tr>
-                      <th scope="col">Title</th>
-                      <th scope="col">Sku</th>
-                      <th className="text-center" scope="col">
+                      <th scope='col'>Title</th>
+                      <th scope='col'>Sku</th>
+                      <th className='text-center' scope='col'>
                         Unit Price
                       </th>
-                      <th className="text-center" scope="col">
+                      <th className='text-center' scope='col'>
                         Qty
                       </th>
                     </tr>
                   </thead>
                   <tbody>
                     {data.orderItems.map((product: ShipmentOrderItem, key) => (
-                      <tr key={key} className="border-bottom py-2">
-                        <td className="w-50 fs-6 fw-semibold">
-                          {product.name || ''}
-                        </td>
-                        <td className="fs-6 text-muted">{product.sku}</td>
-                        <td className="text-center">
-                          $ {product.unitPrice.toFixed(2)}
-                        </td>
-                        <td className="text-center">{product.quantity}</td>
+                      <tr key={key} className='border-bottom py-2'>
+                        <td className='w-50 fs-6 fw-semibold'>{product.name || ''}</td>
+                        <td className='fs-6 text-muted'>{product.sku}</td>
+                        <td className='text-center'>$ {product.unitPrice.toFixed(2)}</td>
+                        <td className='text-center'>{product.quantity}</td>
                       </tr>
                     ))}
                     <tr>
-                      <td className="text-start fs-5 fw-bold text-nowrap">
-                        Total QTY
-                      </td>
+                      <td className='text-start fs-5 fw-bold text-nowrap'>Total QTY</td>
                       <td></td>
                       <td></td>
-                      <td className="text-center fs-5 text-primary">
-                        {data.totalItems}
-                      </td>
+                      <td className='text-center fs-5 text-primary'>{data.totalItems}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -186,16 +157,19 @@ const ReturnType = ({ data, apiMutateLink }: Props) => {
         </Col>
       </Row>
       <Row>
-        <Col xl={12} className="d-flex justify-content-end align-items-end">
-          <Card className="m-0">
-            <Button
-              color="secondary"
-              className="btn-label"
-              onClick={() => handlePrintingLabel()}
-            >
-              <i className="las la-toilet-paper label-icon align-middle fs-3 me-2" />
-              {loadingLabel ? <Spinner color="light" /> : 'Print Label'}
-            </Button>
+        <Col xl={12} className='d-flex justify-content-end align-items-end'>
+          <Card className='m-0'>
+            {loadingLabel ? (
+              <Button color='secondary' className='btn-label'>
+                <i className='las la-toilet-paper label-icon align-middle fs-3 me-2' />
+                <Spinner color='light' />
+              </Button>
+            ) : (
+              <Button color='secondary' className='btn-label' onClick={() => handlePrintingLabel()}>
+                <i className='las la-toilet-paper label-icon align-middle fs-3 me-2' />
+                Print Label
+              </Button>
+            )}
           </Card>
         </Col>
       </Row>
