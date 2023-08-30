d4dcebff2f9feb02cc51531ae7656acd96f2abc1
rsourdis <rsourdis@gmail.com>
2023-08-30 15:03:02 -0400

    Changing Create Order to Shopify 1.0

--- src/pages/api/createNewOrder.ts
+++ src/pages/api/createNewOrder.ts
@@ -11,7 +11,7 @@ const createNewOrder: NextApiHandler = async (request, response) => {
         return
     }
 
-    axios.post(`${process.env.API_DOMAIN_SERVICES}/${request.query.region}/api/createNewOrder.php?businessId=${request.query.businessId}`, {
+    axios.post(`${process.env.API_DOMAIN_SERVICES}/${request.query.region}/api/createNewOrderShopify.php?businessId=${request.query.businessId}`, {
         orderInfo: request.body.orderInfo
     })
         .then(({ data }) => {
