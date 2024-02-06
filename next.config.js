d503dfdd27fcc6a0936c087551d3a77dc9f20d89
rsourdis <rsourdis@gmail.com>
2024-02-06 14:37:14 +0100

    Product Perfomance module Fixing Response Payload 0.1

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['electrostoregroup.com','cdn.shopify.com', 'm.media-amazon.com', 't3.ftcdn.net', 'images-na.ssl-images-amazon.com'],
+    domains: ['electrostoregroup.com', 'cdn.shopify.com', 'm.media-amazon.com', 't3.ftcdn.net', 'images-na.ssl-images-amazon.com'],
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/marketplaces/productPerformance',
+        destination: `${process.env.SHELFCLOUD_SERVER_URL}/marketplaces/products/getProductsPerformance`,
+      },
+    ]
   },
 }
 
