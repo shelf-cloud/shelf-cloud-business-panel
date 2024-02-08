5fc05394d8a31d79b46f3b21be28495b017cdc93
rsourdis <rsourdis@gmail.com>
2024-02-08 17:35:08 +0100

    Marketplaces info api call fix 0.1

--- src/pages/api/marketplaces/getMarketplacesInfo.ts
+++ src/pages/api/marketplaces/getMarketplacesInfo.ts
@@ -13,22 +13,28 @@ const getMarketplacesInfo: NextApiHandler = async (request, response) => {
     axios(`${process.env.API_DOMAIN_SERVICES}/${request.query.region}/api/marketplaces/getMarketplacesInfo.php?businessId=${request.query.businessId}`)
         .then(async ({ data }) => {
 
-            const amazonMarketplaces = await axios(`${process.env.SHELFCLOUD_SERVER_URL}/amazon/sellers/getSellerMarketplaces/${request.query.region}/${request.query.businessId}`)
+            await axios(`${process.env.SHELFCLOUD_SERVER_URL}/amazon/sellers/getSellerMarketplaces/${request.query.region}/${request.query.businessId}`)
+                .then((amazonMarketplaces) => {
 
-            if (amazonMarketplaces.data.error || amazonMarketplaces.data.marketplaces.length === 0) {
-                response.json(data)
-                return
-            }
+                    if (amazonMarketplaces.data.error || amazonMarketplaces.data.marketplaces.length === 0) {
+                        response.json(data)
+                        return
+                    }
 
-            for (const marketplaces of amazonMarketplaces.data.marketplaces) {
-                data.marketplaces.unshift({
-                    logo: "https://onixventuregroup.goflow.com/images/channels/amazon.svg",
-                    storeId: marketplaces.marketplaceId,
-                    name: `FBA ${marketplaces.marketplaceName}`,
+                    for (const marketplaces of amazonMarketplaces.data.marketplaces) {
+                        data.marketplaces.unshift({
+                            logo: "https://onixventuregroup.goflow.com/images/channels/amazon.svg",
+                            storeId: marketplaces.marketplaceId,
+                            name: `FBA ${marketplaces.marketplaceName}`,
+                        })
+                    }
+
+                    response.json(data)
                 })
-            }
+                .catch(() => {
+                    response.json(data)
+                })
 
-            response.json(data)
         })
         .catch((error) => {
             if (error.response) {
