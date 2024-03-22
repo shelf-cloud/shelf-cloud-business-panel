ba9041a1f71ee88ad28d6db98964b9f4862853dc
rsourdis <rsourdis@gmail.com>
2024-03-22 17:37:40 -0500

    Adding Sales Over Time 0.1

---
+++ src/components/dashboard/SalesOverTime.tsx
@@ -0,0 +1,77 @@
+import React, { useContext } from 'react'
+// import Link from 'next/link'
+import { Card, CardBody, CardHeader, Col } from 'reactstrap'
+import AppContext from '@context/AppContext'
+import moment from 'moment'
+import CountUp from 'react-countup'
+import SalesOverTimeTimeline from './SalesOverTimeTimeline'
+
+type Props = {
+  salesOverTime: { [key: string]: { [key: string]: number } }
+  loadingData: boolean
+}
+
+const SalesOverTime = ({ salesOverTime, loadingData }: Props) => {
+  const { state }: any = useContext(AppContext)
+  const totalToday = Object.values(salesOverTime[moment().format('YYYY-MM-DD')]).reduce((a, b) => a + b, 0)
+  const totalYesterday = Object.values(salesOverTime[moment().subtract(1, 'days').format('YYYY-MM-DD')]).reduce((a, b) => a + b, 0)
+
+  let salesDiff = 0
+  if (totalToday > 0 && totalYesterday > 0) {
+    salesDiff = ((totalToday - totalYesterday) / totalYesterday) * 100
+  }
+  return (
+    <React.Fragment>
+      <Col>
+        <Card>
+          <CardHeader className='align-items-center d-flex justify-content-between'>
+            <h4 className='card-title mb-0 flex-grow-1'>Sales Over Time</h4>
+            <a className='fs-6 text-muted fw-normal'>{moment().format('h:mm a')}</a>
+          </CardHeader>
+
+          <CardBody className='pb-0'>
+            <div className='d-flex flex-row justify-content-start align-items-base gap-4'>
+              <div>
+                <p className='m-0 p-0 fw-medium'>
+                  Today <span className={'fw-bold fs-6 ' + (salesDiff > 0 ? 'text-success' : 'text-danger')}>{salesDiff.toFixed(2)}%</span>
+                </p>
+                <p className='m-0 p-0 fw-semibold fs-5'>
+                  <span className='counter-value'>
+                    <CountUp
+                      start={0}
+                      prefix={state.currentRegion == 'us' ? '$ ' : ''}
+                      suffix={state.currentRegion == 'eu' ? ' €' : ''}
+                      separator={state.currentRegion == 'us' ? '.' : ','}
+                      end={totalToday}
+                      decimals={2}
+                      duration={1}
+                    />
+                  </span>
+                </p>
+              </div>
+              <div>
+                <p className='m-0 p-0 fw-medium'>Yesterday</p>
+                <p className='m-0 p-0 fw-semibold fs-5'>
+                  <span className='counter-value'>
+                    <CountUp
+                      start={0}
+                      prefix={state.currentRegion == 'us' ? '$ ' : ''}
+                      suffix={state.currentRegion == 'eu' ? ' €' : ''}
+                      separator={state.currentRegion == 'us' ? '.' : ','}
+                      end={totalYesterday}
+                      decimals={2}
+                      duration={1}
+                    />
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div>{loadingData ? 'Loading...' : <SalesOverTimeTimeline salesOverTime={salesOverTime} />}</div>
+          </CardBody>
+        </Card>
+      </Col>
+    </React.Fragment>
+  )
+}
+
+export default SalesOverTime
---
+++ src/components/dashboard/SalesOverTimeTimeline.tsx
@@ -0,0 +1,203 @@
+import React, { useContext } from 'react'
+import AppContext from '@context/AppContext'
+import dynamic from 'next/dynamic'
+import { FormatCurrency } from '@lib/FormatNumbers'
+import { ApexOptions } from 'apexcharts'
+import moment from 'moment'
+const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false })
+
+const TimeLine = [
+  '12 AM',
+  '1 AM',
+  '2 AM',
+  '3 AM',
+  '4 AM',
+  '5 AM',
+  '6 AM',
+  '7 AM',
+  '8 AM',
+  '9 AM',
+  '10 AM',
+  '11 AM',
+  '12 PM',
+  '1 PM',
+  '2 PM',
+  '3 PM',
+  '4 PM',
+  '5 PM',
+  '6 PM',
+  '7 PM',
+  '8 PM',
+  '9 PM',
+  '10 PM',
+  '11 PM',
+]
+
+type Props = {
+  salesOverTime: { [key: string]: { [key: string]: number } }
+}
+const SalesOverTimeTimeline = ({ salesOverTime }: Props) => {
+  const { state }: any = useContext(AppContext)
+
+  const YESTERDAY = moment().subtract(1, 'days').format('YYYY-MM-DD')
+  const TODAY = moment().format('YYYY-MM-DD')
+
+  const series = [
+    {
+      name: 'Today',
+      data: Object?.values(salesOverTime[TODAY]),
+    },
+    {
+      name: 'Yesterday',
+      data: Object?.values(salesOverTime[YESTERDAY]),
+    },
+  ]
+
+  const options = {
+    chart: {
+      zoom: {
+        enabled: true,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    stroke: {
+      width: 2,
+      curve: 'smooth',
+      lineCap: 'round',
+    },
+    markers: {
+      size: 3,
+      colors: ['#3577f1', '#999d9c'],
+    },
+    legend: {
+      onItemClick: {
+        toggleDataSeries: false,
+      },
+    },
+    // plotOptions: {
+    //   bar: {
+    //     horizontal: true,
+    //     borderRadius: 2,
+    //     dataLabels: {
+    //       position: '',
+    //     },
+    //   },
+    // },
+    // dataLabels: {
+    //   enabled: true,
+    //   formatter: function (val) {
+    //     if (val > 0) {
+    //       return FormatCurrency.format(val)
+    //     }
+    //   },
+    //   textAnchor: 'start',
+    //   style: {
+    //     fontSize: '14px',
+    //     fontWeight: 'bold',
+    //     colors: ['#000'],
+    //   },
+    // },
+    colors: ['#3577f1', '#999d9c'],
+    // grid: {
+    //   show: true,
+    //   borderColor: '#000',
+    //   position: 'back',
+    //   xaxis: {
+    //     lines: {
+    //       show: true,
+    //     },
+    //   },
+    //   yaxis: {
+    //     lines: {
+    //       show: false,
+    //     },
+    //   },
+    // },
+    tooltip: {
+      intersect: false,
+      x: {
+        show: true,
+      },
+      y: {
+        // formatter: function (value, { series, seriesIndex, dataPointIndex, w }) {
+        //   return FormatCurrency(state.currentRegion, value)
+        // },
+      },
+    },
+    xaxis: {
+      type: 'category',
+      // tickAmount: Object.keys(timeLineSorted).length / 8,
+      categories: TimeLine,
+      labels: {
+        show: true,
+      },
+    },
+    yaxis: [
+      {
+        seriesName: 'Today',
+        show: true,
+        showAlways: true,
+        labels: {
+          show: true,
+          align: 'left',
+          trim: false,
+          formatter: function (val: number) {
+            return FormatCurrency(state.currentRegion, val)
+          },
+          style: {
+            fontSize: '12px',
+            fontWeight: 300,
+            cssClass: 'chargesTitles',
+          },
+        },
+      },
+      {
+        seriesName: 'Today',
+        show: true,
+        showAlways: true,
+        labels: {
+          show: false,
+          align: 'left',
+          trim: false,
+          formatter: function (val: number) {
+            return FormatCurrency(state.currentRegion, val)
+          },
+          // style: {
+          //   fontSize: '14px',
+          //   fontWeight: 400,
+          //   cssClass: 'chargesTitles',
+          // },
+        },
+      },
+      // {
+      //   opposite: true,
+      //   seriesName: 'Units',
+      //   show: true,
+      //   showAlways: true,
+      //   labels: {
+      //     show: true,
+      //     align: 'left',
+      //     trim: false,
+      //     formatter: function (val) {
+      //       return FormatIntNumber(state.currentRegion, val)
+      //     },
+      //     style: {
+      //       fontSize: '12px',
+      //       fontWeight: 300,
+      //       cssClass: 'chargesTitles',
+      //     },
+      //   },
+      // },
+    ],
+  } as ApexOptions
+
+  return (
+    <>
+      <ApexCharts options={options} series={series} type='line' height={300} />
+    </>
+  )
+}
+
+export default SalesOverTimeTimeline
