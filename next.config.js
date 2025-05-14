de78c528083f173fb7272eeb65262caafca26319
rsourdis <rsourdis@gmail.com>
2025-05-14 13:56:57 +0200

    chore: update next.js to version 15.3.2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['electrostoregroup.com', 'cdn.shopify.com', 'm.media-amazon.com', 't3.ftcdn.net', 'images-na.ssl-images-amazon.com'],
   },
